274858e1ca7a70e16f6dc0a4538a3411fdbda6f3
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-01-29 16:35:41 -0500

    Add python-decouple for env variable management

--- src/dialogues/app.py
+++ src/dialogues/app.py
@@ -1,9 +1,11 @@
 # From the ollama-python readme: https://github.com/ollama/ollama-python
-
+from decouple import config
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='deepseek-r1:latest', messages=[
+MODEL = config('MODEL')
+
+response: ChatResponse = chat(model=MODEL, messages=[
   {
     'role': 'user',
     'content': 'Why is the sky blue?',
