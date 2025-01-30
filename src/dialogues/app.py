674b1a5cf755cc8c0a0d8d5417f85c621aca699b
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-01-30 16:41:43 -0500

    Create Interlocutor class to send queries and return streams

--- src/dialogues/app.py
+++ src/dialogues/app.py
@@ -4,6 +4,7 @@ from ollama import chat
 from ollama import ChatResponse
 
 MODEL = config('MODEL')
+DEFAULT_QUERY = config('DEFAULT_QUERY')
 
 # Non-streaming response:
 # response: ChatResponse = chat(model=MODEL, messages=[
@@ -16,11 +17,31 @@ MODEL = config('MODEL')
 # or access fields directly from the response object
 # print(response.message.content)
 
-stream = chat(
-    model=MODEL,
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
-    stream=True,
-)
+class Interlocutor:
+    def __init__(self, model):
+        self.model = model
 
-for chunk in stream:
-    print(chunk['message']['content'], end='', flush=True)
+    def send_query(self, query):
+        try:
+            stream = chat(
+                model=MODEL,
+                messages=[{'role': 'user', 'content': 'What is justice?'}],
+                stream=True,
+            )
+        except Exception:
+            raise
+
+        return stream
+
+
+client = Interlocutor(model=MODEL)
+
+
+def print_response(chat_client, query):
+    stream = chat_client.send_query(query)
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
+
+
+if __name__ == "__main__":
+    print_response(client, DEFAULT_QUERY)
