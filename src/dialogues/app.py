08704124845d0a4466c43a7d4fd90a80ea0872b8
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-02-05 16:45:45 -0500

    Add error handling

--- src/dialogues/app.py
+++ src/dialogues/app.py
@@ -1,7 +1,9 @@
-# From the ollama-python readme: https://github.com/ollama/ollama-python
+# Adapted from the sample code in the ollama-python readme: https://github.com/ollama/ollama-python
 from decouple import config
+from httpx import ConnectError
 from ollama import chat
 from ollama import ChatResponse
+import sys
 
 MODEL = config('MODEL')
 DEFAULT_QUERY = config('DEFAULT_QUERY')
@@ -17,8 +19,12 @@ class Interlocutor:
                 messages=[{'role': 'user', 'content': 'What is justice?'}],
                 stream=True,
             )
-        except Exception:
-            raise
+        except ConnectError as e:
+            print(f"ConnectError: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            sys.exit(1)
 
         return stream
 
@@ -27,10 +33,17 @@ client = Interlocutor(model=MODEL)
 
 
 def print_response(chat_client, query):
-    stream = chat_client.send_query(query)
-    for chunk in stream:
-        print(chunk['message']['content'], end='', flush=True)
-
+    try:
+        stream = chat_client.send_query(query)
+        for chunk in stream:
+            print(chunk['message']['content'], end='', flush=True)
+    except ConnectError:
+        print("Could not contact the server. Are you sure ollama is running?")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
+    
 
 if __name__ == "__main__":
     print_response(client, DEFAULT_QUERY)
